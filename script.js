814fabfed9310b05df78fb6bac924d8ef3f8567c
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2025-07-17 16:04:51 +0500

    Update script.js
    Implemented Leading Zeros for Single-Digit Numbers (e.g., 01:02:03)

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function clock(){
-    //3° preciso passar isso pro HTML
+  var h = new Date().getHours().toString().padStart(2, '0');
+  var m = new Date().getMinutes().toString().padStart(2, '0');
+  var s = new Date().getSeconds().toString().padStart(2, '0');
 
-  var h = new Date().getHours();
-  var m = new Date().getMinutes();
-  var s = new Date().getSeconds();
-
-  var hours = document.getElementById("hour").innerHTML = h;
-  var minutes = document.getElementById("minutes").innerHTML = m;
-  var seconds = document.getElementById("seconds").innerHTML = s;
-
+  document.getElementById("hour").innerHTML = h;
+  document.getElementById("minutes").innerHTML = m;
+  document.getElementById("seconds").innerHTML = s;
 }
 
-var interval = setInterval(clock, 1000); 
+var interval = setInterval(clock, 1000);
